75faf970278efb300572389d308e5abbe28c154e
Sergey Alekseev <varnie29a@mail.ru>
2013-08-09 23:36:53 +0600

    initial commit

---
+++ code/lib/Character.pm
@@ -0,0 +1,236 @@
+package Character;
+
+use Moose;
+use File::Basename;
+use File::Spec::Functions qw/rel2abs/;
+use Carp qw/croak/;
+use 5.010;
+
+use constant {
+    LOOK_AT_RIGHT => 0,
+    LOOK_AT_LEFT => 1,
+    LOOK_AT_ME => 2
+};
+
+has look_sprites => (
+    is => 'rw',
+    isa => 'ArrayRef[ArrayRef[Num]]',
+    default => sub {[ [0, 32*2, 32, 32], [0, 32, 32, 32], [32, 0, 32, 32] ]}
+);
+
+has map_pos => (
+    is => 'rw',
+    isa => 'ArrayRef[Num]',
+    default => sub{ [0, 0, 32, 32] }
+);
+
+has velocity => (
+    is => 'rw',
+    isa => 'Num',
+    default => 30,
+    required => 1
+);
+
+has sprites => (
+    is => 'ro',
+    isa => 'SDL::Surface',
+    lazy => 1,
+    builder => '_build_sprites'
+);
+
+has sprite_index => (
+    is => 'ro',
+    isa => 'Num',
+    default => 0
+);                               
+
+has step_x => (
+    is => 'rw',
+    isa => 'Num',
+    default => 0
+);
+
+has step_x_speed => (
+    is => 'rw',
+    isa => 'Num',
+    default => 8
+);
+
+has pos => (
+    is => 'rw',
+    isa => 'ArrayRef[Num]',
+    default => sub {[0, 282-32, 32, 32]}
+);
+
+has dt => (
+    is => 'rw',
+    isa => 'Num',
+    default => Time::HiRes::time
+);
+
+has jumping => (
+    is => 'rw',
+    isa => 'Num',
+    default => 0
+);
+
+has screen_w => (
+    is => 'rw',
+    isa => 'Num',
+    required => 1
+);
+
+has screen_h => (
+    is => 'rw',
+    isa => 'Num',
+    required => 1
+);
+
+has map_width => (
+    is => 'rw',
+    isa => 'Num',
+    required => 1
+);
+
+has map_ref => (
+    is => 'rw',
+    isa => 'HashRef[Num]',
+    required => 1
+);
+
+sub get_pos_x {
+    my $self = shift;
+    return $self->pos->[0];
+}
+
+sub get_pos_y {
+    my $self = shift;
+    return $self->pos->[1];
+}
+
+sub reset_velocity {
+    my $self = shift;
+    $self->velocity(30);
+}
+
+sub calc_map_pos {
+    my $self = shift;
+    my ($pos_x, $pos_y) = ($self->get_pos_x, $self->get_pos_y);
+    my $x = (($pos_x < $self->screen_w/2) || ($self->screen_w >= $self->map_width)) ? $pos_x : $pos_x - $self->screen_w/2 > $self->map_width - $self->screen_w ? $pos_x - ($self->map_width - $self->screen_w) : $self->screen_w/2;
+    @{$self->map_pos}[0 .. 1] = ($x, $pos_y);
+    return $self->map_pos;
+}
+
+sub draw {
+    my ($self, $display_surface_ref) = (shift, shift);
+    my $src;
+    if ($self->step_x > 0) {                              
+        $src = $self->look_sprites->[LOOK_AT_RIGHT];
+        $src->[0] = 32*$self->sprite_index;
+    } elsif ($self->step_x < 0) {
+        $src = $self->look_sprites->[LOOK_AT_LEFT];
+        $src->[0] = 32*$self->sprite_index;
+    } else {
+        $src = $self->look_sprites->[LOOK_AT_ME];
+    }
+
+    ${$display_surface_ref}->blit_by($self->sprites, $src, $self->calc_map_pos);
+}
+
+sub update_pos {
+    my ($self, $new_dt) = (shift, shift);
+    my ($x, $y) = ($self->pos->[0], $self->pos->[1]);  
+
+    if ($self->step_x != 0) {
+        my $new_x = $x + $self->step_x_speed*$self->step_x;       
+        if ($new_x >= 0 && $new_x <= $self->map_width-32) { 
+            if ($self->step_x == 1) {
+                if (!$self->is_map_val($new_x+32, $y)) {
+                    $self->pos->[0] = $new_x;
+                } else {
+                    $self->pos->[0] = 32*(int($new_x/32));    
+                }
+            } else {
+                if (!$self->is_map_val($new_x, $y)) {
+                    $self->pos->[0] = $new_x;
+                } else {
+                    $self->pos->[0] = 32*(int($new_x/32)+1);
+                }
+            }
+        
+            if (!$self->jumping && !$self->is_map_val($self->pos->[0], $self->pos->[1]+1)) {
+                $self->jumping(1);
+                $self->velocity(0);
+            }
+        }
+    } 
+
+    if ($self->jumping) {
+        # failing
+        if (!$self->velocity) { 
+            say "FAILING";
+
+            if ($self->is_map_val($self->pos->[0], $self->pos->[1]) || $self->is_map_val($self->pos->[0]+31, $self->pos->[1])) {
+                $self->jumping(0);
+            } else {
+                my $new_y = int($y + (5*9.81*(($new_dt - $self->dt)**2))/2); 
+                my $test_y = $self->pos->[1];
+                my $catched_thru_pass = 0;
+                my $diff = $self->screen_h - 768;
+                while ($test_y < $new_y) {
+                    if ($self->is_map_val($self->pos->[0], $test_y+32) || $self->is_map_val($self->pos->[0]+31, $test_y+32)) {
+                        $self->pos->[1] = $test_y - ($test_y - $diff)%32;
+                        $self->jumping(0);
+
+                        $catched_thru_pass = 1;
+                        last;
+                    } else {
+                        $test_y += 8;
+                    }
+                }
+
+                if (!$catched_thru_pass) {
+                    $self->pos->[1] = $new_y;
+                }
+            }
+        } else {
+            # jumping up
+            say "UP";
+            my $new_velocity = $self->velocity - 9.81;
+            if ($new_velocity < 0) {
+                $new_velocity = 0;
+            }
+
+            my $new_y = $self->pos->[1] - $new_velocity;
+            if (!$self->is_map_val($self->pos->[0], $new_y) && !$self->is_map_val($self->pos->[0]+31, $new_y)) {
+                $self->pos->[1] = $new_y; 
+                $self->velocity($new_velocity);
+            } else { 
+                $self->jumping(0);
+                $self->velocity(0);
+            }
+        }
+    }
+}
+
+sub is_map_val {
+    my ($self, $x, $y) = (shift, shift, shift);
+    return ($y >= $self->screen_h-768) && $self->map_ref->{int($x/32)*24 + int(($y - ($self->screen_h-768))/32)};
+}
+
+sub update_index {
+    my $self = shift;
+    if (++$self->{sprite_index} == 3) {
+        $self->{sprite_index} = 0;
+    }
+}
+
+sub _build_sprites {
+    my $res = SDL::Image::load(dirname(rel2abs($0)) . '/../tiles/RE1_Sprites_v1_0_by_DoubleLeggy.png');
+    croak(SDL::get_error) unless ($res);
+    return $res;
+}
+
+no Moose;
+__PACKAGE__->meta->make_immutable;
+1;
---
+++ code/yup.pl
@@ -0,0 +1,164 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use SDL;
+use SDL::Events;
+use SDLx::App;
+use SDL::Image;
+use SDL::Video;
+use SDL::Surface;
+use SDL::Rect;
+use SDL::VideoInfo;
+use SDL::PixelFormat;
+
+use Time::HiRes;
+use Carp qw/croak/;
+
+use File::Basename;
+use File::Spec::Functions qw/rel2abs/;
+
+use FindBin;
+use lib "$FindBin::Bin/lib";
+
+use Character;
+use 5.010;
+
+SDL::init(SDL_INIT_VIDEO);
+
+my $video_info = SDL::Video::get_video_info;
+my ($screen_w, $screen_h, $bits_per_pixel) = ($video_info->current_w, $video_info->current_h, $video_info->vfmt->BitsPerPixel);
+
+my $display = SDL::Video::set_video_mode($screen_w, $screen_h, $bits_per_pixel, SDL_HWSURFACE|SDL_DOUBLEBUF|SDL_FULLSCREEN);
+my $display_surface = SDLx::Surface->new(surface => $display);
+my $display_surface_ref = \$display_surface;
+
+#test
+my $tiles_surface = SDL::Image::load(dirname(rel2abs($0)) . '/../tiles/JnRTiles.png'); 
+croak(SDL::get_error) unless ($tiles_surface);
+
+my $sky_surface = SDL::Image::load(dirname(rel2abs($0)) . '/../tiles/cloud.jpg');
+croak(SDL::get_error) unless ($sky_surface);
+
+my ($map_ref, $max_x) = create_map();
+my %map = %$map_ref;
+
+#say 'total ', scalar keys %map;
+#say ((scalar keys %map) / 24); 
+#test
+
+my ($ch, $e, $quit, $time) = (
+    Character->new(
+        screen_w => $screen_w,
+        screen_h => $screen_h, 
+        map_width => $max_x, 
+        map_ref => $map_ref, 
+        jumping => 1, 
+        velocity => 0
+    ), 
+    SDL::Event->new, 
+    0, 
+    Time::HiRes::time
+);
+
+my $FRAME_RATE = 1000/25;
+
+while (!$quit) {
+    SDL::Events::pump_events();
+    while (SDL::Events::poll_event($e)) {
+        my $key_sym;
+        if ($e->type == SDL_KEYDOWN) {
+            $key_sym = $e->key_sym;
+            if ($key_sym == SDLK_ESCAPE) {
+                $quit = 1;
+            } elsif ($key_sym == SDLK_RIGHT) {
+                $ch->step_x(1);
+            } elsif ($key_sym == SDLK_LEFT) {
+                $ch->step_x(-1);
+            } elsif ($key_sym == SDLK_UP) {
+                if (!$ch->jumping) {
+                    $ch->reset_velocity;
+                    $ch->jumping(1);
+                    $ch->dt($time);
+                }
+            }
+        } elsif ($e->type == SDL_KEYUP) {
+            $key_sym = $e->key_sym;
+            if (($key_sym == SDLK_RIGHT && $ch->step_x == 1) || ($key_sym == SDLK_LEFT && $ch->step_x == -1)) {
+                $ch->step_x(0);
+            }
+        }
+    }
+    
+    my $frames_cnt = 0;
+    my $start_ticks = SDL::get_ticks();
+    my $new_time = Time::HiRes::time;
+    my $bg_fill_color = SDL::Color->new(0x36, 0x9A, 0xD5);
+    
+    if ($new_time - $time > 0.01) {
+        $display_surface->draw_rect([0, $sky_surface->h, $screen_w, $screen_h], $bg_fill_color);
+        
+        my $ch_pos_x = $ch->get_pos_x;     
+        my $map_offset;
+        my $sky_offset;
+        if ($ch_pos_x < $screen_w/2 || $screen_w >= $max_x) {
+            $sky_offset = $map_offset = 0;
+        } elsif ($ch_pos_x - $screen_w/2 > $max_x - $screen_w) {
+            $sky_offset = $max_x - $screen_w;
+            $map_offset = $sky_offset / 32;
+        } else { 
+            $sky_offset = $ch_pos_x - $screen_w/2;
+            $map_offset = $sky_offset / 32;
+        }
+
+        my ($len, $x) = (0, 0);
+        while ($len < $screen_w) {
+            my $cur_len = $sky_surface->w - $sky_offset;
+            $cur_len = $screen_w-$x unless ($x+$cur_len <= $screen_w);
+            $display_surface->blit_by($sky_surface, [$sky_offset, 0, $cur_len, $sky_surface->h], [$x, 0, $x+$cur_len, $sky_surface->h]);
+            $sky_offset = 0;
+            $len += $cur_len;
+            $x += $cur_len;
+        }
+        
+        my $tile_rect = [0, 0, 32, 32];
+        my $y_offset = $screen_h - 768;
+        foreach my $x ($map_offset..$map_offset+$screen_w/32) {
+            my $val = $x*24; #(int($x/32)*32 + $x%32)*24;
+            foreach my $y (0 .. 24) {
+                if (exists $map{$val + $y}) {
+                    $display_surface->blit_by($tiles_surface, $tile_rect, [($x-$map_offset)*32, $y_offset + 32*$y, 32, 32]);
+                }
+            }
+        }
+                        
+        $new_time = Time::HiRes::time;
+        $ch->update_index;
+        $ch->update_pos($new_time);
+        $ch->draw($display_surface_ref);
+        
+        $display_surface->flip;
+
+        my $diff = SDL::get_ticks() - $start_ticks;
+        if ($FRAME_RATE > $diff) {
+            SDL::delay($FRAME_RATE-$diff);
+        }
+
+        $time = $new_time;
+        say "FPS: ", (++$frames_cnt/$diff)*1000;
+    }
+}
+
+sub create_map {
+    my %map; 
+    foreach my $x (0..1024*3 -1) {
+#        if ($x%24 == 23) {
+        if (($x%24 != 22 && $x%24 != 21 && $x%24 != 18 && $x%24 != 17 && $x%24 != 17 && $x%24 != 16 && $x%24 != 16) && ($x == 23*4 || $x == 0 || $x == 23 || $x == 71 || $x == 58 || $x == 69 ||  $x == 84  || $x==119 || $x > 120 && $x != 769 && $x != 770)) {
+            $map{$x} = 1;
+        }
+    }        
+    $map{94} = 1;
+    
+    return (\%map, 1024*3);
+}
