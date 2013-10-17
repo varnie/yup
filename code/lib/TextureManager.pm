005f83c60edae4d7116717b6e20a455b6d4212d6
varnie <varnie29a@mail.ru>
2013-10-18 01:04:18 +0600

    Update TextureManager.pm
    rewrote work with relative/absolute path

--- code/lib/TextureManager.pm
+++ code/lib/TextureManager.pm
@@ -7,20 +7,20 @@ use warnings;
 use Mouse;
 use base 'Class::Singleton';
 use File::Basename;
-use File::Spec::Functions qw/rel2abs/;
+use File::Spec;
 use Carp qw/croak/;
 
 use SDL::Image;
 
 use constant TEXTURE_NAMES => do {
-    my $dirname = dirname(rel2abs($0));
+    my ($vol, $dir, $file) = File::Spec->splitpath(dirname($0));
     {
-        WATER => $dirname . '/../tiles/waterstrip11.png',
-        MAIN_CHARACTER =>  $dirname .'/../tiles/RE1_Sprites_v1_0_by_DoubleLeggy.png',
-        TILES => $dirname .'/../tiles/JnRTiles.png',
-        CLOUDS => $dirname .'/../tiles/cloud_new1.png',
-        MOUNTAINS => $dirname .'/../tiles/mountains_new1.png',
-        FOREST => $dirname .'/../tiles/forest_new.png'
+        WATER => File::Spec->catfile($vol, $dir, 'tiles', 'waterstrip11.png'),
+        MAIN_CHARACTER =>  File::Spec->catfile($vol, $dir, 'tiles', 'RE1_Sprites_v1_0_by_DoubleLeggy.png'),
+        TILES => File::Spec->catfile($vol, $dir, 'tiles', 'JnRTiles.png'),
+        CLOUDS => File::Spec->catfile($vol,  $dir, 'tiles', 'cloud_new1.png'),
+        MOUNTAINS => File::Spec->catfile($vol, $dir, 'tiles', 'mountains_new1.png'),
+        FOREST => File::Spec->catfile($vol, $dir, 'tiles', 'forest_new.png')
     }
 };
 
